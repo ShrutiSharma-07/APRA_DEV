061a426b65f97cc60347951e004b95a59dc7e95e
Shrutis070 <shruti.sharma2@students.mq.edu.au>
2025-05-21 14:38:07 +1000

    Requiremenrs_2_0 Title matching complete

---
+++ Finalised Title matching/SQL Scripts.sql
@@ -0,0 +1,95 @@
+--TITLE MATCHING
+CREATE OR REPLACE TABLE adc_works_exact_match_title_variants AS (
+  WITH cte1 AS (
+    SELECT 
+      apra_work_id,
+      apra_cleaned_title,
+      UPPER(REGEXP_REPLACE(APRA_ISWC, '[^A-Za-z0-9]', '')) AS apra_iswc,
+    FROM ADC_WORKS_TITLE_VARIANTS_CLEAN 
+    WHERE apra_cleaned_title IS NOT NULL AND apra_cleaned_title <> ''
+  ),
+  cte2 AS (
+    SELECT 
+      UPPER(muzooka_track_id) AS muzooka_track_id,
+      muzooka_cleaned_title,
+      muzooka_iswc
+    FROM mzk_tracks_title_variants_clean
+    WHERE muzooka_cleaned_title IS NOT NULL AND muzooka_cleaned_title <> ''
+  )
+  SELECT DISTINCT
+    cte1.apra_work_id, 
+    cte1.apra_cleaned_title,
+    cte1.apra_iswc,
+    cte2.muzooka_track_id,
+    cte2.muzooka_cleaned_title,
+    cte2.muzooka_iswc,
+    100 AS match_score,
+    CASE WHEN cte1.apra_iswc = cte2.muzooka_iswc THEN 'Y' ELSE 'N' END AS YN_ISWC_MATCH
+  FROM cte1 
+  JOIN cte2
+  ON UPPER(cte1.apra_cleaned_title) = UPPER(cte2.muzooka_cleaned_title)
+  -- where cte1.apra_work_id = 'GW00577031'
+);
+
+
+
+CREATE OR REPLACE TABLE ADC_WORKS_NON_EXACT_MATCH_TITLE_VARIANTS AS
+SELECT DISTINCT
+  a.apra_work_id,
+  UPPER(a.apra_cleaned_title) AS apra_cleaned_title,
+  UPPER(REGEXP_REPLACE(a.apra_iswc, '[^A-Za-z0-9]', '')) AS apra_iswc
+FROM adc_works_title_variants_clean a
+WHERE a.apra_cleaned_title <> '' -- Filter empty titles first for better performance
+AND NOT EXISTS (
+  SELECT 1 
+  FROM mzk_tracks_title_variants_clean m
+  WHERE UPPER(a.apra_cleaned_title) = UPPER(m.muzooka_cleaned_title)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- COMPOSERS
+
+create or replace TABLE EDW_APPS.MATCHING.ADC_WORKS_TRACKS_MATCHED_COMPOSERS AS (
+SELECT
+	A.ADC_COMPOSER_ID, 
+	A.APRA_WORK_ID, 
+	A.IPI, 
+	UPPER(A.NAME) AS COMPOSER_NAME,
+	F.MUZOOKA_TRACK_ID 
+FROM ADCCOMPOSERS A
+INNER JOIN FUZZY_TITLE_MATCHING F
+ON A.APRA_WORK_ID = F.APRA_WORK_ID);
+
+
+
+create or replace TABLE EDW_APPS.MATCHING.MZK_TRACKS_WORKS_MATCHED_COMPOSERS AS (
+SELECT DISTINCT
+	C.COMPOSER_ID,
+    UPPER(C.TRACK_ID) AS MUZOOKA_TRACK_ID,
+    UPPER(C.COMPOSER) AS COMPOSER_NAME,
+	C.IPI, 
+FROM COMPOSERS C
+INNER JOIN FUZZY_TITLE_MATCHING F
+ON UPPER(C.TRACK_ID) = F.MUZOOKA_TRACK_ID);
+
+
+
+
+
+
+
+
+
+
